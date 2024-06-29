0be75ad249cf78853d0a212b9b7067199df4482e
Aditi <as3300789@gmail.com>
2024-06-29 08:45:48 +0530

    2nd_day_of_project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Home from './screens/Home';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import React from 'react';
 import Login from './screens/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
 
 function App() {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -8,12 +8,21 @@ function Card() {
                     <div className="card-body">
                          <h5 className="card-title">Card title</h5>
                          <p className="card-text">Some quick example text to build on the card title.</p>
-                         <div>
-                              <select className='m-2 h-100 w-100 bg-success'>
+                         <div className='container w-100'>
+                              <select className='h-100 bg-success rounded'>
                                    {Array.from(Array(6), (e, i) => (                             
                                              <option key={i+1} value={i+1}> {i+1}</option>
                                    ))}
                               </select>
+
+                              <select className='m-2 h-100 bg-success rounded'>
+                                   <option value="half">Half</option>
+                                   <option value="full">Full</option>
+                              </select>
+
+                              <div className='d-inline h-100 fs-6'>
+                                   Total Price
+                              </div>
                          </div>
                     </div>
                </div>
